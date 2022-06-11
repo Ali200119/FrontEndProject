d6e46e04d1f850389acac3d4fcaf2179f3d0a4ac
Ali200119 <aliit@code.edu.az>
2022-06-12 00:13:44 +0400

    Almost complete Products Part2 Cards

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -4,7 +4,7 @@ let minutes = document.getElementById("Minutes");
 let seconds = document.getElementById("Seconds");
 let currentYear = new Date().getFullYear();
 
-let endTime = new Date (`June 19 ${currentYear} 00:00:00`)
+let endTime = new Date (`June 20 ${currentYear} 00:00:00`)
 
 function updateCountdownTime(){
     let currentTime = new Date();
